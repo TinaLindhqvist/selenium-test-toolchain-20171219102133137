ae658efc03d1744aa1811c02ce8e633025b10895
Christina Lindhqvist <christina.lindhqvist@se.ibm.com>
2017-12-19 13:59:16 +0000

    test my page

--- tests/test_python_search.py
+++ tests/test_python_search.py
@@ -8,15 +8,8 @@ class GoogleSearch(unittest.TestCase):
         self.driver = webdriver.Firefox()
 
     def test_search_in_google(self):
-        self.driver.get("http://www.google.com")
-        self.assertIn("Google", self.driver.title)
-        elem = self.driver.find_element_by_name("q")
-        elem.send_keys("bluemix")
-        elem.send_keys(Keys.RETURN)
-        self.driver.implicitly_wait(15)
-        new_elem = self.driver.find_elements_by_class_name("r");
-        assert "Wikipedia" in new_elem[0].text
-
+        self.driver.get("https://clouddeveloperapp.mybluemix.net")
+        self.assertIn("Cloud Developer", self.driver.title)
 
     def tearDown(self):
         self.driver.close()
